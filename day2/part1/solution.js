ba1519eb9b3bf365539a1b4ae37ff64a49047e4b
Gerard Bove <gerardbovetous@gmail.com>
2023-12-07 18:35:25 +0100

    day2 part1

---
+++ day2/part1/solution.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require('fs');
+const data = readFileSync('./input', 'utf-8').split(/\r?\n/);
+
+const redCubes = 12;
+const greenCubes = 13;
+const blueCubes = 14;
+
+let counter = 0;
+
+const splitIntoSets = data.map(game => game.split("; "))
+
+function isPossible(game) {
+    let isPossible = true;
+    game.forEach(gameSet => {
+        const redIndex = gameSet.search("red") - 3;
+        const greenIndex = gameSet.search("green") - 3;
+        const blueIndex = gameSet.search("blue") - 3;
+        const redNum = Number(gameSet.substring(redIndex, redIndex + 2));
+        const greenNum = Number(gameSet.substring(greenIndex, greenIndex + 2));
+        const blueNum = Number(gameSet.substring(blueIndex, blueIndex + 2))
+        if (redNum > redCubes || greenNum > greenCubes || blueNum > blueCubes) {
+            isPossible = false;
+        }
+    })
+    return isPossible;
+}
+
+splitIntoSets.forEach((game, index) => {
+    if (isPossible(game)) {
+        counter += index+1;
+    }
+})
+
+console.log("RESULT: ", counter)
